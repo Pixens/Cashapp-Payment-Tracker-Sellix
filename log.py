a55b165f53410500d7c002eb26db1385f5cb055e
Pixens <107793019+Pixens@users.noreply.github.com>
2023-05-19 19:13:22 +0530

    Create log.py

---
+++ log.py
@@ -0,0 +1,18 @@
+import datetime, os
+from colorama import Fore, Style
+
+os.system("")
+    
+
+def info(level:str, content: str = "", extra: dict = {}):
+    print(message)
+    
+def debug(level:str, content: str = "", extra: dict = {}):
+    print(message)
+    
+
+    
+def error(level:str, content: str = "", extra: dict = {}):
+    print(message)
+
+
